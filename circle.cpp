9b596f8cc349353af0f6cdaffe2b19ce11f761a8
nengkya <nengkya@yahoo.com>
2023-09-13 14:56:41 +0700

    if (event.type == sf::Event::Closed)

--- circle.cpp
+++ circle.cpp
@@ -4,7 +4,10 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(333, 333), "HaGa IT Solution");
     while (window.isOpen()) {
 	sf::Event event;
-	    
-
+	while (window.pollEvent(event)) {	
+	    if (event.type == sf::Event::Closed) {
+		window.close();
+	    }
+	}
     }
 }
