5f8e8bed561e9b9c4210e7cd7cf10f8463a02b84
nengkya <nengkya@yahoo.com>
2023-09-13 21:01:46 +0700

    sf::CircleShape shape(100.f);

--- circle.cpp
+++ circle.cpp
@@ -2,12 +2,23 @@
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(333, 333), "HaGa IT Solution");
+    sf::CircleShape circle(100.f);
+    circle.setFillColor(sf::Color::Cyan);
+
     while (window.isOpen()) {
 	sf::Event event;
+
 	while (window.pollEvent(event)) {	
 	    if (event.type == sf::Event::Closed) {
 		window.close();
 	    }
+	
+	/* clear maximize strech */
+	window.clear();
+	
+	/* draw to buffer, not screen */
+	window.draw(circle);
+	window.display();
 	}
     }
 }
